425887bfaf60aa52a48f598c44b0ea462b025c91
Hoang Dong <hoangdongfit@gmail.com>
2019-11-04 22:24:38 +0700

    file

---
+++ src/FindMaxInArray.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class FindMaxInArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter size: ");
+            size = input.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20");
+            }
+        } while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = input.nextInt();
+            i++;
+        }
+
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (max < array[j]) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+
+        System.out.println("Max in array is: " + max + " and location: " + index);
+    }
+}
